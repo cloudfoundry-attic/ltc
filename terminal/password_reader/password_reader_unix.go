5af86f1ccba31de6a4e2e22902f46d85b0c3eee0
Keaty Gross <kgross@pivotal.io>
2015-09-30 11:55:57 -0500

    WIP
    Signed-off-by: Damon Anderson <danderson@pivotal.io>

--- terminal/password_reader/password_reader_unix.go
+++ terminal/password_reader/password_reader_unix.go
@@ -8,68 +8,25 @@ import (
 	"fmt"
 	"os"
 	"strings"
-	"syscall"
 )
-
-const (
-	sttyArg0   = "/bin/stty"
-	exec_cwdir = ""
-)
-
-// Tells the terminal to turn echo off.
-var sttyArgvEOff []string = []string{"stty", "-echo"}
-
-// Tells the terminal to turn echo on.
-var sttyArgvEOn []string = []string{"stty", "echo"}
-
-var ws syscall.WaitStatus = 0
 
 func (pr passwordReader) PromptForPassword(promptText string, args ...interface{}) (passwd string) {
-
 	// Display the prompt.
 	fmt.Printf(promptText+": ", args...)
 
-	// File descriptors for stdin, stdout, and stderr.
-	fd := []uintptr{os.Stdin.Fd(), os.Stdout.Fd(), os.Stderr.Fd()}
-
-	pr.exitHandler.OnExit(func() { echoOn(fd) })
-
-	pid, err := echoOff(fd)
-	defer echoOn(fd)
-	if err != nil {
-		return
-	}
-
-	passwd = readPassword(pid)
+	passwd = readPassword()
 
 	fmt.Println("") // Carriage return after the user input.
 
 	return
 }
 
-func readPassword(pid int) string {
+func readPassword() string {
 	rd := bufio.NewReader(os.Stdin)
-	syscall.Wait4(pid, &ws, 0, nil)
 
 	line, err := rd.ReadString('\n')
 	if err == nil {
 		return strings.TrimSpace(line)
 	}
 	return ""
-}
-
-func echoOff(fd []uintptr) (int, error) {
-	pid, err := syscall.ForkExec(sttyArg0, sttyArgvEOff, &syscall.ProcAttr{Dir: exec_cwdir, Files: fd})
-	if err != nil {
-		return 0, fmt.Errorf("failed turning off console echo for password entry:\n{{.ErrorDescription}}", map[string]interface{}{"ErrorDescription": err})
-	}
-
-	return pid, nil
-}
-
-func echoOn(fd []uintptr) {
-	pid, e := syscall.ForkExec(sttyArg0, sttyArgvEOn, &syscall.ProcAttr{Dir: exec_cwdir, Files: fd})
-	if e == nil {
-		syscall.Wait4(pid, &ws, 0, nil)
-	}
 }
