3fbabfccece67fa0a1597e3322e83a688fc469e9
Mark DeLillo <mdelillo@pivotal.io>
2015-10-26 16:22:15 -0500

    Use filepath.Join() for Windows support
    [Finishes #104804478]
    
    Signed-off-by: Damon Anderson <danderson@pivotal.io>

--- config/config_helpers/config_helpers_test.go
+++ config/config_helpers/config_helpers_test.go
@@ -1,6 +1,8 @@
 package config_helpers_test
 
 import (
+	"path/filepath"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -10,8 +12,8 @@ import (
 var _ = Describe("ConfigHelpers", func() {
 	Describe("ConfigFileLocation", func() {
 		It("returns the config location for the diego home path", func() {
-			fileLocation := config_helpers.ConfigFileLocation("/home/chicago")
-			Expect(fileLocation).To(Equal("/home/chicago/.lattice/config.json"))
+			fileLocation := config_helpers.ConfigFileLocation(filepath.Join("home", "chicago"))
+			Expect(fileLocation).To(Equal(filepath.Join("home", "chicago", ".lattice", "config.json")))
 		})
 	})
 })
